03569336c024f51eeb4ce57ad09c421cecbd79ea
avishek1001 <1706avsihek@gmail.com>
2025-05-21 20:07:28 +0530

    first route setup

---
+++ server/routes/businessProfile.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import BusinessProfile from '../models/businessProfile.js'
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const newProfile = new BusinessProfile(req.body);
+        const saveProfile = await newProfile.save();
+
+        res.status(201).json(saveProfile);
+    } catch (err) {
+        console.error("POST / error: ", err);
+        res.status(500).json({ message: "Could not create profile" })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const profile = await BusinessProfile.findById(id)
+            .populate("businesses")
+            .populate("contacts")
+
+        if(!profile) {
+            return res.status(404).json({message: "Profile cannot be found"});
+        }
+
+        res.status(200).json(profile);
+    } catch(err) {
+        console.error("GET / error: ", err);
+        res.status(500).json({message: "Server error!"})
+    }
+})
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -3,24 +3,27 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+
+import businessProfile from './routes/businessProfile.js'
+
 dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log("MongoDB connection error", err))
 
 const app = express();
 
 // Middleware
 app.use(cors({
     origin: 'http://localhost:5173',
-    credentials: true // only if using cookies or sessions
+    // credentials: true // only if using cookies or sessions
 }));
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log("MongoDB connection error", err))
-
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+// Routes
+app.use('/api/business-profile', businessProfile);
 
 app.listen(process.env.PORT, () => {
     console.log("Listening from port 5000");
